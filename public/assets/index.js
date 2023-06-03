116adbaa421235e4e381d668722e9d4ae16e57a7
ri-commits <kartashourychard@gmail.com>
2023-06-03 19:09:59 +0300

    images

---
+++ public/assets/index.js
@@ -0,0 +1,23 @@
+import rassvet from "rassvet.webp";
+import arch from "./arch.jpg";
+import ded from "ded.jpg";
+import zubr from "zubr.jpg";
+import tatem from "tatem.jpg";
+import archfront from "./archfront.jpg";
+import doroga from "doroga.jpg";
+import grib from "grib.jpg";
+import kon from "kon.jpg";
+import olen from "olen.jpg";
+
+export default {
+  rassvet: rassvet,
+  arch: arch,
+  ded: ded,
+  zubr: zubr,
+  tatem: tatem,
+  archfront: archfront,
+  doroga: doroga,
+  grib: grib,
+  kon: kon,
+  olen: olen,
+};
