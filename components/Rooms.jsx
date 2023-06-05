43288c59592cc1669259cec7fd2563e3ef7e3245
ri-commits <kartashourychard@gmail.com>
2023-06-05 11:23:49 +0300

    Rooms styled

--- components/Rooms.jsx
+++ components/Rooms.jsx
@@ -1,5 +1,22 @@
 import Image from "next/image.js";
 import images from "../public/assets/rooms/index.js";
+import cloud from "../public/assets/cloud.png";
+import Link from "next/link.js";
+
+const ReservationButton = () => (
+  <Link
+    href="/contacts"
+    className="mt-3 group text-gray-800 py-4 md:py-5 px-8 md:px-10 relative overflow-hidden"
+  >
+    <span
+      className="absolute inset-0 bg-cover bg-center transition-opacity opacity-0  group-hover:opacity-100"
+      style={{ backgroundImage: `url(${cloud.src})` }}
+    ></span>
+    <span className="relative z-10 font-caveat md:text-3xl text-2xl group-hover:text-white">
+      Забронировать
+    </span>
+  </Link>
+);
 
 const Room = ({ image, description, reverse }) => {
   return (
@@ -18,7 +35,8 @@ const Room = ({ image, description, reverse }) => {
         />
       </div>
       <div className="flex flex-col justify-center items-center p-5 text-gray-800 md:w-1/2">
-        <h2 className="md:text-3xl text-2xl font-caveat">{description}</h2>
+        <h2 className="md:text-6xl text-3xl font-caveat">{description}</h2>
+        <ReservationButton />
       </div>
     </div>
   );
@@ -35,16 +53,19 @@ const Rooms = () => {
         image={images.room1}
         description="Охотничий уголок"
         reverse={false}
+        key="room1"
       />
       <Room
         image={images.room2}
         description="Арабская ночь (семейный)"
         reverse={true}
+        key="room2"
       />
       <Room
         image={images.room3}
         description="В гостях у Жюль Верна"
         reverse={false}
+        key="room3"
       />
     </div>
   );
